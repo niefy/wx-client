b7295fe647c71916ac2e66e60f63eb1e850e9fc2
niefy <niefy@qq.com>
2020-06-19 15:14:33 +0800

    支持多公众号

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <router-view/>
   </div>
 </template>
+<script>
+import cookie from './js/cookie'
+const APPID = process.env.VUE_APP_WX_APPID;
+export default {
+    mounted(){
+        cookie.set('appid',APPID)
+    }
+}
+</script>
 <style src="./style/common.css" scope></style>
 <style>
 .empty-banner{
